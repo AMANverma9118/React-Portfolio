bad09200c8f8cb02dd9a8adcddf77c48a4554360
AMANverma9118 <AMANverma9118359330@gmail.com>
2024-10-26 00:17:05 +0530

    Tech Symbol is completed

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,7 +8,6 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
@@ -18,7 +17,6 @@ import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
 import meta from "./company/meta.png";
@@ -40,7 +38,6 @@ export {
   menu,
   close,
   css,
-  docker,
   figma,
   git,
   html,
@@ -50,7 +47,6 @@ export {
   reactjs,
   redux,
   tailwind,
-  typescript,
   threejs,
   meta,
   shopify,
