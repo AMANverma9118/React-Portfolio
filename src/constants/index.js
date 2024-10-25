476a49b53652ac12495e19c585e6f49e8dce8a7f
AMANverma9118 <AMANverma9118359330@gmail.com>
2024-10-25 23:21:15 +0530

    About section created with animation

--- src/constants/index.js
+++ src/constants/index.js
@@ -50,11 +50,11 @@ import {
       icon: mobile,
     },
     {
-      title: "Backend Developer",
+      title: "Frontend Developer",
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Backend Developer",
       icon: creator,
     },
   ];
